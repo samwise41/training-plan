ecf88422c9f332c183288ddafd1842be20c6122a
samwise41 <134550253+samwise41@users.noreply.github.com>
2026-01-09 08:52:26 -0700

    Update fetch_garmin.py

--- python/fetch_garmin.py
+++ python/fetch_garmin.py
@@ -6,6 +6,7 @@ from garminconnect import Garmin
 # --- CONFIGURATION ---
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 JSON_FILE = os.path.join(SCRIPT_DIR, 'my_garmin_data_ALL.json')
+FETCH_LIMIT = 50  # Number of recent activities to fetch
 
 # --- CREDENTIALS ---
 # Get secrets from GitHub Environment
@@ -89,5 +90,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
